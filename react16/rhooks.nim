06c060948ffcd3a3e3ab1dd29f199bee30b5daae
Kristian Mandrup <kmandrup@gmail.com>
2020-02-19 11:26:15 +0000

    add basic support for React Router hooks

--- react16/rhooks.nim
+++ react16/rhooks.nim
@@ -7,12 +7,14 @@ type
   Reference* = JsObject
     current*: auto
 
-proc useState*(): seq[auto] {. importjs "React.useState" .}
 proc useState*(initialState: State): seq[auto] {. importjs "React.useState" .}
+
 proc useEffect*(didUpdate: proc(), triggerDependencies: seq[auto]) {. importjs "React.useEffect" .}
+
 proc useContext*(context: JsObject): Context {. importjs "React.useContext" .}
-proc useReducer*(reducer: proc(), initialArg: auto) {. importjs "React.useReducer" .}
+
 proc useReducer*(reducer: proc(), initialArg: auto, init: proc()) {. importjs "React.useReducer" .}
+
 proc useCallback*(callback: proc(), triggerDependencies: seq[auto]) {. importjs "React.useCallback" .}
 proc useMemo*(createFn: proc(), recomputeDependencies: seq[auto]) {. importjs "React.useMemo" .}
 proc useRef*(): Reference {. importjs "React.useRef" .}
---
+++ react16/rrouter.nim
@@ -0,0 +1,23 @@
+
+import macros, jsffi
+import react
+
+type
+  History* = JsObject
+
+  Location* = JsObject
+    pathname*: string
+  RouteMatch* = JsObject
+  path*: string
+  strict*: bool
+  sensitive*: bool
+
+{.push importjs.}  
+proc useLocation(): Location
+proc useHistory(): History
+proc useParams[T](): T
+proc useRouteMatch(RouteMatch): auto
+
+proc push(history: History, path: string)
+proc goBack(history: History)
+proc goForward(history: History)
